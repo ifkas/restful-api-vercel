c7c541cf9246fc49ff29284fd6d977ce05f7d122
IVOCULIC\Ivo Culic <ivo.culic@sopro.io>
2022-12-14 14:06:57 +0100

    Small update to the README

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
+	// Add custom route here if needed
 	jsonServer.rewriter({
 		"/api/*": "/$1",
 	})
